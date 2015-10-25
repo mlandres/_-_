524ee579bc4ecae703334eb19a22bdfdfa03beec
Michael Andres <ma@suse.de>
2015-10-25 18:02:23 +0100

    zosc

---
+++ zosc/cmd.py
@@ -0,0 +1,34 @@
+#======================================================================
+import cout
+import select
+import subprocess
+#======================================================================
+
+def _readByLine( stream ):
+  while True:
+    line = stream.readline()
+    if line: yield line[:-1]	# wo. NL
+    else: break
+
+#======================================================================
+
+def run( *args ):
+  p = subprocess.Popen( args, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+  reads = [p.stdout.fileno(), p.stderr.fileno()]
+
+  while reads:
+    if p.poll() is None:
+      ret = select.select( reads, [], [] )
+    else:
+      ret = [ reads ]
+      reads = None
+
+    for fd in ret[0]:
+      if fd == p.stdout.fileno():
+	for line in _readByLine( p.stdout ):
+	  yield line
+      if fd == p.stderr.fileno():
+	for line in _readByLine( p.stderr ):
+	  cout.eRED( '*** ', line )
+
+#======================================================================
--- zosc/osc.py
+++ zosc/osc.py
@@ -1,22 +1,84 @@
-import subprocess
+#======================================================================
+import cout
+import cmd
+import re
 #======================================================================
 
-def ls():
-  return 'hi'
+class Osc():
 
-def _ls():
-  return 'hi'
+  def ls( self, rx = None ):
+    for line in self._cmd( 'ls' ):
+      if rx and not re.match( rx, line ): continue
+      yield Prj( self, line )
+
+  @classmethod
+  def _cmd( cls, *args ):
+    for line in cmd.run( cls._cmdstr(), *args ):
+      yield line
+
+  @classmethod
+  def _cmdstr( cls ):
+    return 'unknown'
+
+  @classmethod
+  def __str__( cls ):
+    return cls._cmdstr()
 
 #======================================================================
-class Pkg():
 
-  def __init__( self ):
-    pass
+class Obs( Osc ):
+
+  @classmethod
+  def _cmdstr( cls ):
+    return 'osc'
+
+  @classmethod
+  def __str__( cls ):
+    return 'obs:/'
 
 #======================================================================
+
+class Ibs( Osc ):
+
+  @classmethod
+  def _cmdstr( cls ):
+    return 'isc'
+
+  @classmethod
+  def __str__( cls ):
+    return 'ibs:/'
+
+#======================================================================
+
 class Prj():
 
-  def __init__( self ):
-    pass
+  def __init__( self, osc, name ):
+    self.osc = osc
+    self.name = name
+
+  def ls( self, rx = None ):
+    for line in self.osc._cmd( 'ls', self.name ):
+      if rx and not re.match( rx, line ): continue
+      yield Pkg( self, line )
+
+  def __str__( self ):
+    return self.name
+
+#======================================================================
+
+class Pkg():
+
+  def __init__( self, prj, name  ):
+    self.prj = prj
+    self.name = name
+
+  def ls( self, rx = None ):
+    for line in self.prj.osc._cmd( 'ls', '-u', self.prj.name, self.name ):
+      if rx and not re.match( rx, line ): continue
+      yield Pkg( self, line )
+
+  def __str__( self ):
+    return self.name
+
 
 #======================================================================
