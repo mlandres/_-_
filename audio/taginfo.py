45b90cb8943e0f09cf27a1d62dd058eeabdfac73
Michael Andres <ma@suse.de>
2019-04-09 09:48:20 +0200

    taginfo ready

--- audio/taginfo.py
+++ audio/taginfo.py
@@ -1,26 +1,27 @@
 #! /usr/bin/python3
 
+import argparse
 import sys, os
 import taglib
 
 class Opt:
-    all = False
-    fs  = 24 if all else 13
+    tags = True
+    all  = False
+    fs   = 24 if all else 15
     deft = [
-        'TITLE',
-        'ARTIST',
-        'LENGTH',
-        'ALBUM',
         'ALBUMARTIST',
+        'ALBUM',
         'DISCNUMBER',
         'TRACKNUMBER',
-        'DATE',
+        'ARTIST',
+        'TITLE',
+        'LENGTH',
         'ORIGINALYEAR',
-        'GENRE'
         ]
+    ret = 0
 
 def ptag( tag, val ):
-    print( "%-*s:" % ( Opt.fs, tag ), val )
+    print( '%-*s:' % ( Opt.fs, tag ), val )
 
 def ptagif( tag, val ):
     ptag( tag, '' if val is None else val )
@@ -28,8 +29,14 @@ def ptagif( tag, val ):
 
 def taginfo( file ):
     song = taglib.File( file )  # might throw
-    ptag( "FILE",  file )
 
+    ptag( 'file',  file )
+    ptag( 'audio', '%3d kbs, %d Hz, %d ch, %2d:%02d min.' % ( song.bitrate, song.sampleRate, song.channels, song.length/60 , song.length%60 ) )
+
+    if not Opt.tags:
+        return
+
+    print( '---------------------------------------------------------' )
     if Opt.all:
         for t in song.tags:
             ptag( t, song.tags[t] )
@@ -42,16 +49,26 @@ def taginfo( file ):
 def pfile( file ):
     if not os.path.isfile( file ):
         return
-    print( "=========================================================" )
+    print( '=============================================================================' )
     try:
         taginfo( file )
     except Exception as e:
-        print( "*** ",  file )
+        print( '*** ',  file )
         print( e )
+        Opt.ret = 1
 
 
 def main():
-    for arg in sys.argv[1:]:
+    parser = argparse.ArgumentParser( description='Print audio properties and textual tags of one or more audio files.' )
+    parser.add_argument( '-n', '--notags',  help='print no tags',  action='store_true' )
+    parser.add_argument( '-a', '--alltags', help='print all tags', action='store_true' )
+    parser.add_argument('file', nargs='+', help='file(s) to print tags of' )
+    args = parser.parse_args()
+
+    Opt.tags = not args.notags
+    Opt.all  = args.alltags
+
+    for arg in args.file:
         if os.path.isdir( arg ):
             dirc = os.listdir( arg )
             dirc.sort()
@@ -60,8 +77,8 @@ def main():
         else:
             pfile( arg )
 
-    return 0
+    return Opt.ret
 
 
 if __name__ == '__main__':
-    main()
+    exit( main() )
